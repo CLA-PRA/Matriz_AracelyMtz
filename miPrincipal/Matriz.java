1e5b8fc6c67889342c79e76554f898b87443e582
AracelyMtz <89017086+AracelyMtz@users.noreply.github.com>
2023-08-05 17:43:05 -0700

    v2 CLA Matriz

--- miPrincipal/Matriz.java
+++ miPrincipal/Matriz.java
@@ -11,43 +11,30 @@ public class Matriz {
                     }
         }
         public int[][] suma (int [][] A, int [][] B){
-               // columnas de la matriz A
-               int n= A[0].length;
-               // filas de la matriz A
+
                int m= A.length;
-               // filas de la matriz B
-               int n2= B.length;
-               // columnas de la matriz B
                int o= B[0].length;
-               // nueva matriz 
                int [][] C= new int [m][o];
                           
                for (int j = 0; j < A.length; j++) {
                     for (int k = 0; k < B.length; k++) {
                      // aquí se multiplica la matriz
-                        C[j][k] += A[j][k] + B[j][k];
+                        
                       }
                    }
                return C;
             }
         
         public int[][] multiplica (int [][] A, int [][] B){
-               // columnas de la matriz A
-               int n= A[0].length;
-               // filas de la matriz A
                int m= A.length;
-               // filas de la matriz B
-               int n2= B.length;
-               // columnas de la matriz B
                int o= B[0].length;
-               // nueva matriz 
                int [][] C= new int [m][o];
                           
                for (int i = 0; i < A.length; i++) {
                     for (int j = 0; j < B.length; j++) {
                         for (int k = 0; k < A.length; k++) {
                             // aquí se multiplica la matriz
-                            C[i][j] += A[i][k] * B[k][j];
+
                         }
                     }
                 }
